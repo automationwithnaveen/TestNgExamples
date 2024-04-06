ee2c37a324a5cec76b18cfcb2b9e8d8f4e124390
Naveen <Naveen@Naveens-MacBook-Pro-2.local>
2024-04-06 09:44:45 +0100

    Day 25

---
+++ src/loan/carloan/newcar/Registration.java
@@ -0,0 +1,24 @@
+package loan.carloan.newcar;
+
+import org.testng.annotations.Test;
+
+public class Registration 
+{
+	@Test
+	public void webRegistration()
+	{
+		System.out.println("NewCar Web Registration");
+	}
+	
+	@Test
+	public void mobileRegistration()
+	{
+		System.out.println("NewCar Mobile Registration");
+	}
+	
+	@Test
+	public void apiRegistration()
+	{
+		System.out.println("NewCar API Registration");
+	}
+}
---
+++ src/loan/carloan/oldcar/Registration.java
@@ -0,0 +1,24 @@
+package loan.carloan.oldcar;
+
+import org.testng.annotations.Test;
+
+public class Registration 
+{
+	@Test
+	public void webRegistration()
+	{
+		System.out.println("OLdCar Web Registration");
+	}
+	
+	@Test
+	public void mobileRegistration()
+	{
+		System.out.println("OLdCar Mobile Registration");
+	}
+	
+	@Test
+	public void apiRegistration()
+	{
+		System.out.println("OLdCar API Registration");
+	}
+}
